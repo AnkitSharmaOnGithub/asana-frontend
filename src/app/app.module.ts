f7e7d4f36fa9e376860eb877970eb164945660ea
Ankit Sharma <ankitsharma101299@gmail.com>
2021-04-22 12:03:57 +0530

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { ViewTaskComponent } from './tasks/view-task/view-task.component';
+import { CreateEditTaskComponent } from './tasks/create-edit-task/create-edit-task.component';
+import { SignupComponent } from './signup/signup.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LoginComponent, TasksComponent, ViewTaskComponent, CreateEditTaskComponent, SignupComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
